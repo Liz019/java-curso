b53bccf7d71ca958fe0d26d6b02f72d69123e8bf
Liz019 <86498117+Liz019@users.noreply.github.com>
2021-07-07 19:57:35 -0300

    Add files via upload

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,17 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		System.out.println(salario);
+		
+		System.out.println("Meu salario é " + salario);
+				
+		int soma = (int) (5.0 / 2);
+		System.out.println(soma);
+		
+		double divisao = 5.0 / 2;
+		System.out.println(divisao);
+	}
+}
