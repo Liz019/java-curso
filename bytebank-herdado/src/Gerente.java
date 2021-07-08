5f5e0d5939ceb2ea20d5618ac591fd243d057e61
Liz019 <86498117+Liz019@users.noreply.github.com>
2021-07-08 15:47:41 -0300

    Add files via upload

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,7 +1,11 @@
 
 public class Gerente extends Funcionario implements Autenticavel {
 		
-	private int senha;
+	private Autenticador autenticador;
+	
+	public Gerente() {
+		this.autenticador = new Autenticador();
+	}
 		
 	public double getBonificacao() {
 		System.out.println("Chamando o método de bonicacao do Gerente");
@@ -10,16 +14,12 @@ public class Gerente extends Funcionario implements Autenticavel {
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
+		return this.autenticador.autentica(senha);
 	}
 	
 }
