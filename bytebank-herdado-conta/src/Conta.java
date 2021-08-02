b32f84610f06155138fd3fb7319ed816a7ba68c9
Luiz Felipe Biazio Aquino <86498117+Liz019@users.noreply.github.com>
2021-08-02 18:23:25 -0300

    ....

--- bytebank-herdado-conta/src/Conta.java
+++ bytebank-herdado-conta/src/Conta.java
@@ -21,7 +21,7 @@ public abstract class Conta {
 	
 	public abstract void deposita(double valor);
 	
-	public void saca(double valor) {
+	public void saca(double valor) throws SaldoInsuficienteException {
 		if(this.saldo < valor ) {
 			throw new SaldoInsuficienteException("saldo: " + this.saldo + ", Valor: " + valor);
 			
@@ -30,7 +30,7 @@ public abstract class Conta {
 		this.saldo -= valor;
 	}
 	
-	public void transfere(double valor, Conta destino) { 
+	public void transfere(double valor, Conta destino) throws SaldoInsuficienteException { 
 	    saca(valor);
 	    destino.deposita(valor);
 	}
